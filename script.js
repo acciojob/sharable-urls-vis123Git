be2488b706fb341d5fd7d50ce7c7b965fadeaccf
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-11-13 19:14:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// your code here
+const updateFun = ()=>{
+	const name = document.getElementById("name").value
+	const year = document.getElementById("year").value
+	const url = document.getElementById("url")
+	url.innerText  = `https://localhost:8080/?${name ? "name=" : "" }${name ? name : "" }${(name && year) ? "&" : "" }${year ? "year=" : "" }${year ? year : "" }`
+
+}
